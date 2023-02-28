de329b3d153ed2348f4096837e2b946295265c77
smmartprogrammer <partabrajp@gmail.com>
2023-02-28 22:57:31 +0500

    session turnury operator added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,32 +1,32 @@
 import { getServerSession } from "next-auth";
+import Login from "../components/Login";
 import { SessionProvider } from "../components/SessionProvider";
 import SideBar from "../components/SideBar";
 import { authOptions } from "../pages/api/auth/[...nextauth]";
 import "../styles/globals.css";
-
-
-
 
 export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-
-const session = await getServerSession(authOptions)
+  const session = await getServerSession(authOptions);
 
   return (
     <html lang="en">
       <body>
         <SessionProvider session={session}>
-        <div className="flex">
-          <div className="bg-[#202123] max-w-xs h-screen overflow-y-auto md:min-w-[20rem]  ">
-          <SideBar />
-          </div>
-          
+          {!session ? (
+            <Login />
+          ) : (
+            <div className="flex">
+              <div className="bg-[#202123] max-w-xs h-screen overflow-y-auto md:min-w-[20rem]  ">
+                <SideBar />
+              </div>
 
-          <div className="bg-[#343541] flex-1 ">{children}</div>
-        </div>
+              <div className="bg-[#343541] flex-1 ">{children}</div>
+            </div>
+          )}
         </SessionProvider>
       </body>
     </html>
