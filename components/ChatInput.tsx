03de87f5acafdd167d5b4ff6a22a26f50f6015a8
smmartprogrammer <partabrajp@gmail.com>
2023-03-02 03:03:50 +0500

    working in chatinput functionality

--- components/ChatInput.tsx
+++ components/ChatInput.tsx
@@ -1,8 +1,11 @@
 "use client";
 
 import { PaperAirplaneIcon } from "@heroicons/react/24/solid";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
 import { useSession } from "next-auth/react";
 import { useState } from "react";
+import { db } from "../firebase";
+
 
 type Props = {
   chatId: string;
@@ -12,9 +15,33 @@ function ChatInput({ chatId }: Props) {
   const [prompt, setPrompt] = useState("");
   const { data: session } = useSession();
 
+  const sentMessage = async () => {
+    e.preventDefault();
+    if (!prompt) return;
+
+    const input = prompt.trim();
+    setPrompt("");
+
+    const message: Message = {
+      text: input,
+      createdAt: serverTimestamp(),
+      user: {
+        _id: session?.user?.email!,
+        name: session?.user?.name,
+        avatar:
+          session?.user?.image! ||
+          `https://ui-avatars.com/api/?name=${session?.user?.name} `,
+      },
+    };
+
+    await addDoc(
+      collection(db, 'users',        session?.user?.email!,        'chats',        chatId,        'messages')
+    );
+  };
+
   return (
     <div className="bg-gray-700/50 text-gray-400 rounded-lg text-sm ">
-      <form className="p-5 space-x-5 flex">
+      <form onSubmit={sentMessage} className="p-5 space-x-5 flex">
         <input
           className="bg-transparent focus:outline-none flex-1 disabled:cursor-not-allowed disabled:text-gray-300 "
           disabled={!session}
@@ -23,8 +50,11 @@ function ChatInput({ chatId }: Props) {
           type="text"
           placeholder="Type your message here... "
         />
-        <button disabled={!prompt || !session}  type="submit" 
-        className="bg-[#11A37F] hover:opacity-50 text-white font-bold px-4 py-2 rounded disabled:bg-gray-300 disabled:cursor-not-allowed ">
+        <button
+          disabled={!prompt || !session}
+          type="submit"
+          className="bg-[#11A37F] hover:opacity-50 text-white font-bold px-4 py-2 rounded disabled:bg-gray-300 disabled:cursor-not-allowed "
+        >
           <PaperAirplaneIcon className="h-4 w-4 -rotate-45" />
         </button>
       </form>
