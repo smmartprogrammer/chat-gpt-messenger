07e8e4d545d133696b6ea776b52175f28028ddd7
smmartprogrammer <partabrajp@gmail.com>
2023-03-02 02:09:32 +0500

    chat input ui made

--- components/ChatInput.tsx
+++ components/ChatInput.tsx
@@ -1,17 +1,36 @@
-type Props ={
-    chatId : string
-}
+"use client";
 
+import { PaperAirplaneIcon } from "@heroicons/react/24/solid";
+import { useSession } from "next-auth/react";
+import { useState } from "react";
 
-function ChatInput({chatId}:Props) {
+type Props = {
+  chatId: string;
+};
+
+function ChatInput({ chatId }: Props) {
+  const [prompt, setPrompt] = useState("");
+  const { data: session } = useSession();
+
   return (
-    <div>
-        <form>
-            <input type="text" />
-        </form>
-        
-</div>
-  )
+    <div className="bg-gray-700/50 text-gray-400 rounded-lg text-sm ">
+      <form className="p-5 space-x-5 flex">
+        <input
+          className="bg-transparent focus:outline-none flex-1 disabled:cursor-not-allowed disabled:text-gray-300 "
+          disabled={!session}
+          value={prompt}
+          onChange={(e) => setPrompt(e.target.value)}
+          type="text"
+          placeholder="Type your message here... "
+        />
+        <button disabled={!prompt || !session}  type="submit" 
+        className="bg-[#11A37F] hover:opacity-50 text-white font-bold px-4 py-2 rounded disabled:bg-gray-300 disabled:cursor-not-allowed ">
+          <PaperAirplaneIcon className="h-4 w-4 -rotate-45" />
+        </button>
+      </form>
+      <div>{/* model selection */}</div>
+    </div>
+  );
 }
 
-export default ChatInput
+export default ChatInput;
