0513fa48d59167a9a31ae4f8efff0bde1a7a7166
smmartprogrammer <partabrajp@gmail.com>
2023-03-04 02:37:43 +0500

    worked on chat.tsx

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -1,12 +1,39 @@
+"use client";
 
-type Props ={
-    chatId : string
+import { collection, orderBy, query } from "firebase/firestore";
+import { useSession } from "next-auth/react";
+import { useCollection } from "react-firebase-hooks/firestore";
+import { db } from "../firebase";
+
+type Props = {
+  chatId: string;
+};
+
+function Chat({ chatId }: Props) {
+  const { data: session } = useSession();
+
+  const [messages] = useCollection(
+    session &&
+      query(
+        collection(
+          db,
+          "users",
+          session?.user?.email!,
+          "chats",
+          chatId,
+          "messages"
+        ),
+        orderBy("createdAt", "asc")
+      )
+  );
+
+  return <div className="flex-1">
+    
+    {messages?.docs.map((message)=> (
+      <Message key={message.id} message={message.data()}  />
+    ))}
+    
+    </div>;
 }
 
-function Chat({chatId}:Props) {
-  return (
-    <div className="flex-1">Chat</div>
-  )
-}
-
-export default Chat
+export default Chat;
